c58a839422832cf28152499271f18b1452a66494
DIVYXNSH18 <divyansh202ff@gmail.com>
2026-09-23 16:10:21 +0530

    Delete frontend directory

--- frontend/src/app/svgs/svg-icon3.tsx
+++
@@ -1,7 +0,0 @@
-export default function Icon3({ cid }: { cid?: string }) {
-  return (
-    <svg className="w-auto h-[1.0625rem] block overflow-hidden align-middle focus:outline-clr-9 focus:[outline-style:auto] focus:outline-[5px]" data-component="icon" aria-hidden="true" fill="none" height="17" stroke="currentColor" viewBox="0 0 24 24" width="17" xmlns="http://www.w3.org/2000/svg" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-cid={cid}>
-      <path d="m9 18 6-6-6-6" />
-    </svg>
-  );
-}
--- frontend/src/lib/site.ts
+++
@@ -1,1 +0,0 @@
-export const SITE_ORIGIN = (process.env.NEXT_PUBLIC_SITE_ORIGIN ?? "").replace(/\/$/, "");
